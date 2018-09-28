a69661051942bf9e6b48b15cc0892298807df235
lidong <1184064819@qq.com>
2018-09-28 15:03:34 +0800

    电饭锅电饭锅电饭锅电饭锅的

---
+++ src/main/java/com/demaxiya/dama/DamaApplication.java
@@ -0,0 +1,12 @@
+package com.demaxiya.dama;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DamaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DamaApplication.class, args);
+    }
+}
